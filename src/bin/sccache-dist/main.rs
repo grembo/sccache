grembo/sccache

commit 5032a8303cc263ba7331c5b47807a4da9ff31a15

Increase the MSRV and bump dependencies (#1137)
* Bump the MSRV to 1.58

Bumps the official MSRV to 1.58.
This is likely more than required the upcoming version bumps.

* Upgrade cargo zstd to 0.10

* Bump serial_test to v0.6

Changelog does not mention any breaking changes.

* Bump blake to v1

* Bump rouille to 3.5

Required to bump syslog to v6, since otherwise no common version
can be selected for `time`

* Bump syslog to v6

No changelog available. Depends on rouille bump to 3.5, to get a
common version of ´time´.

* Upgrade JWT to v8

The explicit insecure decode method was removed and replaced by
an option for the Validation struct.
The `exp` field had to be added to JobJwt, since otherwise the validation would fail at runtime.
Apparently the `exp` field is required now, even if the valdiation of `exp` is turned off.

src/bin/sccache-dist/main.rs
@@ -265,7 +265,15 @@ fn create_jwt_server_token(
     jwt::encode(header, &ServerJwt { server_id }, &key).map_err(Into::into)
 }
 fn dangerous_insecure_extract_jwt_server_token(server_token: &str) -> Option<ServerId> {
-    jwt::dangerous_insecure_decode::<ServerJwt>(server_token)
+    let validation = {
+        let mut validation = jwt::Validation::default();
+        validation.validate_exp = false;
+        validation.validate_nbf = false;
+        validation.insecure_disable_signature_validation();
+        validation
+    };
+    let dummy_key = jwt::DecodingKey::from_secret(b"secret");
+    jwt::decode::<ServerJwt>(server_token, &dummy_key, &validation)
         .map(|res| res.claims.server_id)
         .ok()
 }
@@ -344,14 +352,12 @@ fn run(command: Command) -> Result<i32> {
                     if secret_key.len() != 256 / 8 {
                         bail!("Size of secret key incorrect")
                     }
-                    let validation = jwt::Validation {
-                        leeway: 0,
-                        validate_exp: false,
-                        validate_nbf: false,
-                        aud: None,
-                        iss: None,
-                        sub: None,
-                        algorithms: vec![jwt::Algorithm::HS256],
+                    let validation = {
+                        let mut validation = jwt::Validation::new(jwt::Algorithm::HS256);
+                        validation.leeway = 0;
+                        validation.validate_exp = false;
+                        validation.validate_nbf = false;
+                        validation
                     };
                     Box::new(move |server_token| {
                         check_jwt_server_token(server_token, &secret_key, &validation)

.github/workflows/ci.yml
@@ -24,7 +24,7 @@ jobs:
         uses: ./.github/actions/rust-toolchain
         with:
           components: ${{ matrix.component }}
-          toolchain: 1.58.1
+          toolchain: 1.58
 
       - name: clippy version
         run: cargo clippy --version
@@ -42,24 +42,24 @@ jobs:
       fail-fast: false
       matrix:
         include:
-          - os: ubuntu-18.04
-            rustc: 1.48.0 # Oldest supported version, keep in sync with README.md
-          - os: ubuntu-18.04
-            rustc: 1.48.0
+          - os: ubuntu-20.04
+            rustc: 1.58.0 # Oldest supported version, keep in sync with README.md
+          - os: ubuntu-20.04
+            rustc: 1.58.0
             extra_desc: dist-server
             extra_args: --no-default-features --features=dist-tests test_dist_ -- --test-threads 1
-          - os: ubuntu-18.04
+          - os: ubuntu-20.04
             rustc: stable
-          - os: ubuntu-18.04
+          - os: ubuntu-20.04
             rustc: beta
-          - os: ubuntu-18.04
+          - os: ubuntu-20.04
             rustc: nightly
             allow_failure: true
             extra_args: --features=unstable
           - os: ubuntu-22.04
           - os: macOS-10.15
           - os: windows-2019
-            rustc: 1.48.0 # Oldest supported version, keep in sync with README.md
+            rustc: 1.58.0 # Oldest supported version, keep in sync with README.md
           - os: windows-2019
             rustc: nightly
             allow_failure: true
@@ -172,7 +172,7 @@ jobs:
       fail-fast: false
       matrix:
         include:
-          - os: ubuntu-18.04
+          - os: ubuntu-20.04
             rustc: nightly
             allow_failure: true
             extra_args: --features=unstable

Cargo.toml
@@ -9,6 +9,7 @@ readme = "README.md"
 categories = ["command-line-utilities", "development-tools::build-utils"]
 keywords = ["ccache"]
 edition = "2018"
+rust-version = "1.58"
 
 [[bin]]
 name = "sccache"
@@ -24,7 +25,7 @@ async-trait = "0.1"
 atty = "0.2.6"
 base64 = "0.13"
 bincode = "1"
-blake3 = "0.3"
+blake3 = "1"
 byteorder = "1.0"
 bytes = "1"
 chrono = { version = "0.4", optional = true }
@@ -41,7 +42,7 @@ http = "0.2"
 hyper = { version = "0.14", optional = true, features = ["server"] }
 hyperx = { version = "1.0", optional = true }
 jobserver = "0.1"
-jsonwebtoken = { version = "7", optional = true }
+jsonwebtoken = { version = "8", optional = true }
 lazy_static = "1.0.0"
 libc = "0.2.10"
 linked-hash-map = "0.5"
@@ -79,14 +80,14 @@ walkdir = "2"
 # by default which pulls in an outdated failure version
 which = { version = "4", default-features = false }
 zip = { version = "0.5", default-features = false }
-zstd = "0.6"
+zstd = "0.10"
 
 # dist-server only
 crossbeam-utils = { version = "0.8", optional = true }
 libmount = { version = "0.1.10", optional = true }
 nix = { version = "0.23.1", optional = true }
-rouille = { version = "~3.4", optional = true, default-features = false, features = ["ssl"] }
-syslog = { version = "5", optional = true }
+rouille = { version = "3.5", optional = true, default-features = false, features = ["ssl"] }
+syslog = { version = "6", optional = true }
 void = { version = "1", optional = true }
 version-compare = { version = "0.1.0", optional = true }
 
@@ -98,7 +99,7 @@ itertools = "0.10"
 predicates = "=2.1.1"
 thirtyfour_sync = "0.27"
 once_cell = "1.9"
-serial_test = "0.5"
+serial_test = "0.6"
 wiremock = "0.4.9"
 
 [target.'cfg(unix)'.dependencies]

src/bin/sccache-dist/token_check.rs
@@ -74,9 +74,10 @@ impl EqCheck {
 const MOZ_SESSION_TIMEOUT: Duration = Duration::from_secs(60 * 15);
 const MOZ_USERINFO_ENDPOINT: &str = "https://auth.mozilla.auth0.com/userinfo";
 
-// Mozilla-specific check by forwarding the token onto the auth0 userinfo endpoint
+/// Mozilla-specific check by forwarding the token onto the auth0 userinfo endpoint
 pub struct MozillaCheck {
-    auth_cache: Mutex<HashMap<String, Instant>>, // token, token_expiry
+    // token, token_expiry
+    auth_cache: Mutex<HashMap<String, Instant>>,
     client: reqwest::blocking::Client,
     required_groups: Vec<String>,
 }
@@ -120,14 +121,20 @@ impl MozillaCheck {
             exp: u64,
             sub: String,
         }
+        let mut validation = jwt::Validation::default();
+        validation.validate_exp = false;
+        validation.validate_nbf = false;
         // We don't really do any validation here (just forwarding on) so it's ok to unsafely decode
-        let insecure_token = jwt::dangerous_insecure_decode::<MozillaToken>(token)
+        validation.insecure_disable_signature_validation();
+        let dummy_key = jwt::DecodingKey::from_secret(b"secret");
+        let insecure_token = jwt::decode::<MozillaToken>(token, &dummy_key, &validation)
             .context("Unable to decode jwt")?;
         let user = insecure_token.claims.sub;
         trace!("Validating token for user {} with mozilla", user);
         if UNIX_EPOCH + Duration::from_secs(insecure_token.claims.exp) < SystemTime::now() {
             bail!("JWT expired")
         }
+
         // If the token is cached and not expired, return it
         let mut auth_cache = self.auth_cache.lock().unwrap();
         if let Some(cached_at) = auth_cache.get(token) {
@@ -220,14 +227,14 @@ fn test_auth_verify_check_mozilla_profile() {
     assert!(check_mozilla_profile(
         "ad|Mozilla-LDAP|asayers",
         &["hris_dept_firefox".to_owned()],
-        profile
+        profile,
     )
     .is_ok());
     assert!(check_mozilla_profile("ad|Mozilla-LDAP|asayers", &[], profile).is_ok());
     assert!(check_mozilla_profile(
         "ad|Mozilla-LDAP|asayers",
         &["hris_the_ceo".to_owned()],
-        profile
+        profile,
     )
     .is_err());
 
@@ -356,7 +363,7 @@ impl ValidJWTCheck {
         );
         let mut validation = jwt::Validation::new(header.alg);
         validation.set_audience(&[&self.audience]);
-        validation.iss = Some(self.issuer.clone());
+        validation.set_issuer(&[&self.issuer]);
         #[derive(Deserialize)]
         struct Claims {}
         // Decode the JWT, discarding any claims - we just care about validity

src/dist/http.rs
@@ -98,6 +98,7 @@ mod common {
     #[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
     #[serde(deny_unknown_fields)]
     pub struct JobJwt {
+        pub exp: u64,
         pub job_id: dist::JobId,
     }
 
@@ -396,14 +397,12 @@ mod server {
     const JWT_KEY_LENGTH: usize = 256 / 8;
     lazy_static! {
         static ref JWT_HEADER: jwt::Header = jwt::Header::new(jwt::Algorithm::HS256);
-        static ref JWT_VALIDATION: jwt::Validation = jwt::Validation {
-            leeway: 0,
-            validate_exp: false,
-            validate_nbf: false,
-            aud: None,
-            iss: None,
-            sub: None,
-            algorithms: vec![jwt::Algorithm::HS256],
+        static ref JWT_VALIDATION: jwt::Validation = {
+            let mut validation = jwt::Validation::new(jwt::Algorithm::HS256);
+            validation.leeway = 0;
+            validation.validate_exp = false;
+            validation.validate_nbf = false;
+            validation
         };
     }
 
@@ -603,13 +602,13 @@ mod server {
     }
     impl dist::JobAuthorizer for JWTJobAuthorizer {
         fn generate_token(&self, job_id: JobId) -> Result<String> {
-            let claims = JobJwt { job_id };
+            let claims = JobJwt { exp: 0, job_id };
             let key = jwt::EncodingKey::from_secret(&self.server_key);
             jwt::encode(&JWT_HEADER, &claims, &key)
                 .map_err(|e| anyhow!("Failed to create JWT for job: {}", e))
         }
         fn verify_token(&self, job_id: JobId, token: &str) -> Result<()> {
-            let valid_claims = JobJwt { job_id };
+            let valid_claims = JobJwt { exp: 0, job_id };
             let key = jwt::DecodingKey::from_secret(&self.server_key);
             jwt::decode(token, &key, &JWT_VALIDATION)
                 .map_err(|e| anyhow!("JWT decode failed: {}", e))

commit c5f552d3e2c867a79a55e27982d6a66ad8e361be

Fix sccache-dist server token creation
This was broken in 5032a8303cc2, which correctly stated
in its commit message that "exp" is mandatory in the
new version of jwtwebtoken.

src/bin/sccache-dist/main.rs
@@ -105,6 +105,7 @@ fn check_server_token(server_token: &str, auth_token: &str) -> Option<ServerId> 
 struct ServerJwt {
     exp: u64,
     server_id: ServerId,
+    exp: usize,
 }
 fn create_jwt_server_token(
     server_id: ServerId,
@@ -112,7 +113,7 @@ fn create_jwt_server_token(
     key: &[u8],
 ) -> Result<String> {
     let key = jwt::EncodingKey::from_secret(key);
-    jwt::encode(header, &ServerJwt { exp: 0, server_id }, &key).map_err(Into::into)
+    jwt::encode(header, &ServerJwt { server_id, exp: 0 }, &key).map_err(Into::into)
 }
 fn dangerous_insecure_extract_jwt_server_token(server_token: &str) -> Result<ServerId> {
     let validation = {